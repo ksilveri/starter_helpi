3778ba9f3e07d2eb204d66ec0a95350567dcba3c
Kelly Silveri <ksilveri@udel.edu>
2024-04-18 19:45:53 -0400

    css the detail career quiz

--- src/DetailQuiz.tsx
+++ src/DetailQuiz.tsx
@@ -39,52 +39,60 @@ function DetailQuiz() {
 
     return (
         <div className ="detail-quiz">
-            <h3> Detailed Career Quiz</h3>
+            <Form.Label className="custom-header">Detailed Career Quiz</Form.Label>
+
             <Form.Group controlId="question1">
-                <Form.Label>Describe your ideal work environment.</Form.Label>
+                <Form.Label className="custom-label">1. Describe your ideal work environment.</Form.Label>
                 <Form.Control
+                    className="custom-textbox"
                     value={first}
                     onChange={updateFirst}/>
             </Form.Group>
 
             <Form.Group controlId="question2">
-                <Form.Label>Describe your ideal job.</Form.Label>
+                <Form.Label className="custom-label">2. Describe your ideal job.</Form.Label>
                 <Form.Control
+                    className="custom-textbox"
                     value={second}
                     onChange={updateSecond}/>
             </Form.Group>
 
             <Form.Group controlId="question3">
-                <Form.Label>How do you spend your time?</Form.Label>
+                <Form.Label className="custom-label">3. How do you spend your time?</Form.Label>
                 <Form.Control
+                    className="custom-textbox"
                     value={third}
                     onChange={updateThird}/>
             </Form.Group>
 
             <Form.Group controlId="question4">
-                <Form.Label>What has been your favorite class and why?</Form.Label>
+                <Form.Label className="custom-label">4. What has been your favorite class and why?</Form.Label>
                 <Form.Control
+                    className="custom-textbox"
                     value={fourth}
                     onChange={updateFourth}/>
             </Form.Group>
 
             <Form.Group controlId="question5">
-                <Form.Label>How would you define success?</Form.Label>
+                <Form.Label className="custom-label">5. How would you define success?</Form.Label>
                 <Form.Control
+                    className="custom-textbox"
                     value={fifth}
                     onChange={updateFifth}/>
             </Form.Group>
 
             <Form.Group controlId="question6">
-                <Form.Label>Do you enjoy interacting and/or working with other people?</Form.Label>
+                <Form.Label className="custom-label">6. Do you enjoy interacting and/or working with other people?</Form.Label>
                 <Form.Control
+                    className="custom-textbox"
                     value={sixth}
                     onChange={updateSixth}/>
             </Form.Group>
 
             <Form.Group controlId="question7">
-                <Form.Label>What do you think are your strengths?</Form.Label>
+                <Form.Label className="custom-label">7. What do you think are your strengths?</Form.Label>
                 <Form.Control
+                    className="custom-textbox"
                     value={seventh}
                     onChange={updateSeventh}/>
             </Form.Group>
