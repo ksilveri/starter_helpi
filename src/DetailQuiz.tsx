567e4558104df9a679f5ad87adae815699cbd0c2
Kelly Silveri <ksilveri@udel.edu>
2024-04-18 15:59:15 -0400

    fixed the textbox issue

--- src/DetailQuiz.tsx
+++ src/DetailQuiz.tsx
@@ -5,19 +5,47 @@ import { Form } from 'react-bootstrap';
 
 
 function DetailQuiz() {
-    const [initial, setIntial] = useState<string>('');
-
-    function updateInitial(event: React.ChangeEvent<HTMLInputElement>){
-        setIntial(event?.target.value)
+    const [first, setInitial] = useState<string>('');
+    const [second, setSecond] = useState<string>('');
+    const [third, setThird] =useState<string>('');
+    const [fourth, setFourth] =useState<string>('');
+    const [fifth, setFifth] =useState<string>('');
+    const [sixth, setSixth] =useState<string>('');
+    const [seventh, setSeventh] =useState<string>('');
+    
+    
+    function updateFirst(event: React.ChangeEvent<HTMLInputElement>) {
+        setInitial(event.target.value)
     }
+
+    function updateSecond(event: React.ChangeEvent<HTMLInputElement>) {
+        setSecond(event.target.value)
+    }
+
+    function updateThird(event: React.ChangeEvent<HTMLInputElement>) {
+        setThird(event.target.value)
+    }
+    function updateFourth(event: React.ChangeEvent<HTMLInputElement>) {
+        setFourth(event.target.value)
+    }
+    function updateFifth(event: React.ChangeEvent<HTMLInputElement>) {
+        setFifth(event.target.value)
+    }
+    function updateSixth(event: React.ChangeEvent<HTMLInputElement>) {
+        setSixth(event.target.value)
+    }
+    function updateSeventh(event: React.ChangeEvent<HTMLInputElement>) {
+        setSeventh(event.target.value)
+    }
+
     return (
         <div className ="detail-quiz">
             <h3> Detailed Career Quiz</h3>
             <Form.Group controlId="question1">
                 <Form.Label>Describe your ideal work environment.<Form.Label>
                 <Form.Control
-                    value={initial}
-                    onChange={updateInitial}/>
+                    value={first}
+                    onChange={updateFirst}/>
             </Form.Label>
             </Form.Label>
             </Form.Group>
@@ -25,25 +53,24 @@ function DetailQuiz() {
             <Form.Group controlId="question2">
                 <Form.Label>Describe your ideal job.</Form.Label>
                 <Form.Control
-                    value={initial}
-                    onChange={updateInitial}/>
+                    value={second}
+                    onChange={updateSecond}/>
             </Form.Group>
 
             <Form.Group controlId="question3">
                 <Form.Label>How do you spend your time?<Form.Label>
                 <Form.Control
-                    value={initial}
-                    onChange={updateInitial}/>
+                    value={third}
+                    onChange={updateThird}/>
             </Form.Label>
             </Form.Label>
             </Form.Group>
 
-
             <Form.Group controlId="question4">
                 <Form.Label>What has been your favorite class and why?<Form.Label>
                 <Form.Control
-                    value={initial}
-                    onChange={updateInitial}/>
+                    value={fourth}
+                    onChange={updateFourth}/>
             </Form.Label>
             </Form.Label>
             </Form.Group>
@@ -51,8 +78,8 @@ function DetailQuiz() {
             <Form.Group controlId="question5">
                 <Form.Label>How would you define success?<Form.Label>
                 <Form.Control
-                    value={initial}
-                    onChange={updateInitial}/>
+                    value={fifth}
+                    onChange={updateFifth}/>
             </Form.Label>
             </Form.Label>
             </Form.Group>
@@ -60,8 +87,8 @@ function DetailQuiz() {
             <Form.Group controlId="question6">
                 <Form.Label>Do you enjoy interacting and/or working with other people?<Form.Label>
                 <Form.Control
-                    value={initial}
-                    onChange={updateInitial}/>
+                    value={sixth}
+                    onChange={updateSixth}/>
             </Form.Label>
             </Form.Label>
             </Form.Group>
@@ -69,8 +96,8 @@ function DetailQuiz() {
             <Form.Group controlId="question7">
                 <Form.Label>What do you think are your strengths?<Form.Label>
                 <Form.Control
-                    value={initial}
-                    onChange={updateInitial}/>
+                    value={seventh}
+                    onChange={updateSeventh}/>
             </Form.Label>
             </Form.Label>
             </Form.Group>
