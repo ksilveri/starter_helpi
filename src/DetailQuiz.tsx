1d645b0baf94e3b51894858b00e1f52f26a23deb
Kelly Silveri <ksilveri@udel.edu>
2024-04-18 14:59:58 -0400

    startign detail questions

--- src/DetailQuiz.tsx
+++ src/DetailQuiz.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './quizzes.css';
+import { Form } from 'react-bootstrap';
+
 
 
 function DetailQuiz() {
+    const [initial, setIntial] = useState<string>('test');
+
+    function updateInitial(event: React.ChangeEvent<HTMLInputElement>){
+        setIntial(event?.target.value)
+    }
     return (
         <div className ="detail-quiz">
             <h3> Detailed Career Quiz</h3>
-            <p>Testing future</p>
+            <Form.Group controlId="question1">
+                <Form.Label>textbox<Form.Label>
+                <Form.Control
+                    value={initial}
+                    onChange={updateInitial}/>
+            </Form.Label>
+            </Form.Label>
+            </Form.Group>
+            <Form.Group controlId="question2">
+                <Form.Label>Second Textbook:</Form.Label>
+                <Form.Control
+                    value={initial}
+                    onChange={updateInitial}/>
+            </Form.Group>
         </div>
     );
 }
